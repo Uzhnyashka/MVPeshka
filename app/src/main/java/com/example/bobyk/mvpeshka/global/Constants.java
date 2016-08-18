23b84a8b937dd4002a0f48f3696352a9a14af195
EgorBobyk <egor4k.964@gmail.com>
2016-08-18 14:54:54 +0300

    as

--- app/src/main/java/com/example/bobyk/mvpeshka/global/Constants.java
+++ app/src/main/java/com/example/bobyk/mvpeshka/global/Constants.java
@@ -5,6 +5,6 @@ package com.example.bobyk.mvpeshka.global;
  */
 public class Constants {
 
-    public static final String BASE_API = "http://194.44.253.78:8090";
-    public static final String API_CATEGORY = "/categories/and";
+    public static final String BASE_API = "";
+    public static final String API_CATEGORY = "";
 }
